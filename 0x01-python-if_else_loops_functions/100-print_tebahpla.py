fb35945ac77e7cb6ad02a3f388f3c409353280fc
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-10-31 16:25:34 -0400

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(122, 96, -1):
-    print(chr(i) if i % 2 == 0 else chr(i-32), end='')
+    print("{:c}".format(i - 32) if i % 2 == 0 else "{:c}".format(i), end='')
