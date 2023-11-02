50e6e39aeea93a2e4cc7b6b294556cf73e764eeb
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-11-02 01:25:56 -0400

    5-variable_load.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
-
-if len(argv) == 1:
-    print("0 argument.")
-if len(argv) == 2:
-    print(f"1 argument:")
-    print(f"1: {argv[1]}")
-elif len(argv) > 2:
-    print(f"{len(argv)-1} arguments:")
-    i = 1
-    for n in argv:
-        print(f"{i}: {argv[i]}")
-        i+=1
-        if i > (len(argv) - 1):
-            break
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("0 argument.")
+        if len(argv) == 2:
+            print(f"1 argument:")
+            print(f"1: {argv[1]}")
+        elif len(argv) > 2:
+            print(f"{len(argv)-1} arguments:")
+            i = 1
+            for n in argv:
+                print(f"{i}: {argv[i]}")
+                i+=1
+                if i > (len(argv) - 1):
+                    break
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-if len(argv) == 1:
-    print(0)
-else:
-    n = 1
-    sum = 0
-    for i in argv:
-        sum += int(argv[n])
-        n += 1
-        if n > (len(argv) - 1):
-            break
-    print(sum)
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print(0)
+    else:
+        n = 1
+        sum = 0
+        for i in argv:
+            sum += int(argv[n])
+            n += 1
+            if n > (len(argv) - 1):
+                break
+        print(sum)
