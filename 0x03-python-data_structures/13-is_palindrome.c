b9c120b41b13ae286abec5b1a0b39e9f8e06a97c
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-11-06 02:03:46 -0500

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,65 +1,85 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
-#include <stddef.h>
 
 
 /**
- * reverse_listint - reverses linked list
- * @head: head node
- * Return: pointer to first node in reversed list
- */
-
-listint_t *reverse_listint(listint_t **head)
+  * slistint_length - Counts the number of elements in a linked list
+  * @head: The linked list to count
+  *
+  * Return: Number of elements in the linked list
+  */
+int listint_length(const listint_t *head)
 {
-	listint_t *previous = NULL;
-	listint_t *present = *head;
-	listint_t *next = NULL;
+        int length = 0;
 
-	while (present)
-	{
-		next = present->next;
-		present->next = previous;
-		previous = present;
-		present = next;
-	}
-	return (previous);
+        while (head != NULL)
+        {
+                ++length;
+                head = head->next;
+        }
+
+        return (length);
 }
 
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: head node pointer
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
-
+  * is_palindrome - Checks if a singly linked list is a palindrome
+  * @head: The head of the singly linked list
+  *
+  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slo = *head, *fast = *head, *ptr = *head, *dup = NULL;
+	listint_t *start = NULL, *finish = NULL;
+	unsigned int i = 0, length = 0, length_cycle = 0, length_list = 0;
 
-	if (*head == NULL || (*head)->next == NULL)
+	if (head == NULL)
+		return (0);
+
+	if (*head == NULL)
 		return (1);
-	while (1)
-	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slo->next;
-			break;
-		}
-		slo = slo->next;
-	}
-	reverse_listint(&dup);
 
-	while (dup && ptr)
+	start = *head;
+	length = listint_length(start);
+	length_cycle = length * 2;
+	length_list = length_cycle - 2;
+	finish = *head;
+
+	for (; i < length_cycle; i = i + 2)
 	{
-		if (ptr->n == dup->n)
-		{
-			dup = dup->next;
-			ptr = ptr->next;
-		}
-		else
+		if (start[i].n != finish[length_list].n)
 			return (0);
+
+		length_list = length_list - 2;
 	}
-	if (!dup)
-		return (1);
-	return (0);
+
+	return (1);
+}
+
+/**
+  * get_nodeint_at_index - Gets a node from a linked list
+  * @head: The head of the linked list
+  * @index: The index to find in the linked list
+  *
+  * Return: The specific node of the linked list
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *present = head;
+	unsigned int iteration_times = 0;
+
+	if (head)
+	{
+		while (present != NULL)
+		{
+			if (iteration_times == index)
+				return (present);
+
+			present = present->next;
+			++iteration_times;
+		}
+	}
+
+	return (NULL);
 }
