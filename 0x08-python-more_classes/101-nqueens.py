a0bf0815d76dc420229d5781e1ffe39d6851e873
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-11-27 18:02:11 -0500

    101-nqueens.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+nqueens backtracking program to print coordinates of n-queens
+on an NxN grid that they are all in non-attacking post
+"""
+
+
+from sys import argv
+
+if __name__ == "__main__":
+    post = []
+    if len(argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if argv[1].isdigit() is False:
+        print("N must be a number")
+        exit(1)
+    n = int(argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    for i in range(n):
+        post.append([i, None])
+
+    def already_exists(y):
+        """check that a queen is not in that y value"""
+        for x in range(n):
+            if y == post[x][1]:
+                return True
+        return False
+
+    def reject(x, y):
+        """checks solution"""
+        if (already_exists(y)):
+            return False
+        i = 0
+        while(i < x):
+            if abs(post[i][1] - y) == abs(i - x):
+                return False
+            i += 1
+        return True
+
+    def clear_a(x):
+        """clears the answers from point of failure"""
+        for i in range(x, n):
+            post[i][1] = None
+
+    def nqueens(x):
+        """recursive backtracking function to determine solution"""
+        for y in range(n):
+            clear_a(x)
+            if reject(x, y):
+                post[x][1] = y
+                if (x == n - 1):
+                    print(post)
+                else:
+                    nqueens(x + 1)
+
+    nqueens(0)
