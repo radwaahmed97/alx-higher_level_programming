f970cb36539addcf1a1a84866d952b617d4c49f8
radwaahmed97 <radwa.ahmed95@yahoo.com>
2023-12-05 15:29:35 -0500

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -8,4 +8,7 @@ def class_to_json(obj):
     (list, dictionary, string, integer and boolean)
     for JSON serialization of an object
     """
-    return obj.__dict__
+    result = {}
+    if hasattr(obj, "__dict__"):
+        result = obj.__dict__.copy()
+    return result
