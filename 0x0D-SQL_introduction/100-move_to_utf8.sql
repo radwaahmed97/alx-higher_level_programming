d9cbcbd7f68aeb086f6d350a19aaa9c3aa657735
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-01-16 15:46:45 -0500

    101-avg_temperatures.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,2 @@
+-- converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server.
+ALTER TABLE second_table CONVERT TO CHARACHTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
