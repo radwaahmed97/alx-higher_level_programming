2be90eaf5fec54599f9272f7046d1fc81a84b00d
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-02-13 13:01:21 -0500

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 1; i <= this.height; i++) {
+      let sp = '';
+      for (let j = 1; j <= this.width; j++) {
+        sp += 'X';
+      }
+      console.log(sp);
+    }
+  }
+};
