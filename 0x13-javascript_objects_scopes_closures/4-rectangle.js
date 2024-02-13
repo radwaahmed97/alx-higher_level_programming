f138fca5b336a5f43a82d88fad4ddb1f024a8b33
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-02-13 13:19:47 -0500

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+
+  print () {
+    for (let i = 1; i <= this.height; i++) {
+      let sp = '';
+      for (let j = 1; j <= this.width; j++) {
+        sp += 'X';
+      }
+      console.log(sp);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
