818428f9e193022f682d26071ae4cd737bd45985
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-02-13 12:47:36 -0500

    2- 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (width, height) {
+    if (width > 0 && height > 0) {
+      this.width = width;
+      this.height = height;
+    }
+  }
+}
+module.exports = Rectangle;
