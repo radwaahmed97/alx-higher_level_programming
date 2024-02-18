c9752c94f6b642abb387655d375fd0f0534a7ee3
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-02-18 00:18:23 -0500

    100-relationship_states_cities.py

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""prints the first State object from the database hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    newstate = State(name='California')
+    newcity = City(name='San Francisco')
+    newstate.cities.append(newcity)
+    session.add(newstate)
+    session.add(newcity)
+    session.commit()
