a23d6a1c119e6d2a145a2515fdb2894b646db61c
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-02-18 00:09:51 -0500

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,12 +5,12 @@ class definition of a State and an instance Base = declarative_base()
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+from relationship_state import Base
 
 
 class City(Base):
     """
-    Class with id and name attributes of each city
+    Class with id and name attributes of each state
     """
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
