9e2eab0e7f691df17136b688d9e91242b60f5039
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-03-01 15:26:33 -0500

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+if __name__ == "__main__":
+    import urllib.request as request
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        html = resp.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
