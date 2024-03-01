1013c6a0571f9ffc901194c1d7df6c5da26f0383
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-03-01 15:52:35 -0500

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,6 +3,11 @@
 takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id
 """
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
 if __name__ == "__main__":
     authen = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     resp = requests.get("https://api.github.com/user", authen=authen)
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,6 +3,10 @@
 takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
+import sys
+import requests
+
+
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
     data = {"q": argv[1][0] if len(argv) > 1 else ""}
